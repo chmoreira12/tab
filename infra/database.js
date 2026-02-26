13e441d96f0a8d77ddffb00808e887503ad11dfd
Carlos Henrique <139134638+chmoreira12@users.noreply.github.com>
2026-02-26 18:38:11 +0000

    make `database.js` more robust to error handling

--- infra/database.js
+++ infra/database.js
@@ -1,5 +1,6 @@
 import { Client } from "pg";
 
+//registra credenciais para acesso ao banco
 async function query(object) {
   const client = new Client({
     host: process.env.POSTGRES_HOST,
@@ -8,10 +9,24 @@ async function query(object) {
     database: process.env.POSTGRES_DB,
     password: process.env.POSTGRES_PASSWORD,
   });
+  
+  //abre conexão com o banco
   await client.connect();
-  const result = await client.query(object);
-  await client.end();
-  return result;
+  
+  //tenta fazer a query e retornar o resultado
+  try{
+    const result = await client.query(object);
+    return result;
+
+  } catch (err){
+    console.log(err);
+    //caso dê erro, mostre
+
+  }finally{
+    await client.end();
+    //sempre finalize a conexão, dando certo ou errado
+  }
+  
 }
 
 export default {

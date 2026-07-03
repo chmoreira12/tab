bbe28dbac6c30577f32633bf91d0995f2f176f04
Carlos Henrique <139134638+chmoreira12@users.noreply.github.com>
2026-07-03 02:03:51 +0000

    feat: hash `password` in `user.create`

--- models/user.js
+++ models/user.js
@@ -1,5 +1,6 @@
 import database from "infra/database.js";
 import { ValidationError, NotFoundError } from "infra/errors.js";
+import password from "models/password.js";
 
 async function findOneByUsername(username) {
   const userFound = await runSelectQuery(username);
@@ -33,12 +34,17 @@ async function findOneByUsername(username) {
 }
 
 async function create(userInputValues) {
+  await hashPasswordInObject(userInputValues);
   await validateUniqueEmail(userInputValues.email);
-
   await validateUniqueUsername(userInputValues.username);
 
   const newUser = await runInsertQuery(userInputValues);
   return newUser;
+
+  async function hashPasswordInObject(userInputValues) {
+    const hashedPassword = await password.hash(userInputValues.password);
+    userInputValues.password = hashedPassword;
+  }
 
   async function validateUniqueUsername(username) {
     const results = await database.query({

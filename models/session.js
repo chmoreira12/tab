ae3a9d6a9ba981ea8f39e4354fc7d2acf6eb962d
Carlos Henrique <139134638+chmoreira12@users.noreply.github.com>
2026-07-22 01:17:09 +0000

    feat: implement session deletion

--- models/session.js
+++ models/session.js
@@ -62,7 +62,7 @@ async function findOneValidByToken(sessionToken) {
 async function renew(sessionId) {
   const expiresAt = new Date(Date.now() + EXPIRATION_IN_MILLISECONDS);
 
-  const renewedSessionObject = runUpdateQuery(sessionId, expiresAt);
+  const renewedSessionObject = await runUpdateQuery(sessionId, expiresAt);
   return renewedSessionObject;
 
   async function runUpdateQuery(sessionId, expiresAt) {
@@ -85,11 +85,36 @@ async function renew(sessionId) {
   }
 }
 
+async function expiresById(sessionId) {
+  const expiredSessionObject = await runUpdateQuery(sessionId);
+  return expiredSessionObject;
+
+  async function runUpdateQuery(sessionId) {
+    const results = await database.query({
+      text: `
+        UPDATE
+          sessions
+        SET
+          expires_at = expires_at - interval '1 year',
+          updated_at = NOW() 
+        WHERE
+          id = $1
+        RETURNING
+          *
+        ;`,
+      values: [sessionId],
+    });
+
+    return results.rows[0];
+  }
+}
+
 const session = {
   create,
   EXPIRATION_IN_MILLISECONDS,
   findOneValidByToken,
   renew,
+  expiresById,
 };
 
 export default session;
